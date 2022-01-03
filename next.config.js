ef010b2eef38b673a7e745932bf49b6122423d07
Aziz Muhammad <aziz@xtramilesolutions.com.au>
2022-01-04 02:12:21 +1100

    Add the app

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+})
+
+/** @type {import('next').NextConfig} */
+module.exports = withNextra({
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // i18n: {
+  //   locales: ['en', 'id'],
+  //   defaultLocale: 'en'
+  // }
+  experimental: {
+    optimizeCss: true,
+  },
+})
