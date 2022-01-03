2366c2e0ffce8e2f7a87fb9cc51bb29b8a4168d3
Aziz Muhammad <aziz@xtramilesolutions.com.au>
2022-01-04 03:45:22 +1100

    Disable SWC

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  swcMinify: false,
   // i18n: {
   //   locales: ['en', 'id'],
   //   defaultLocale: 'en'
